c1f00293550f9cf3a6f98903a01b1d4dc770ea54
Jakz-280 <student.160574@worcesterschools.net>
2026-05-01 11:35:40 -0400

    book nook yeaaaaah

---
+++ main.py
@@ -0,0 +1,52 @@
+   # Da plan:
+'''
+list of books
+welcome message
+ask user for input
+ > ask user for book
+ | add to list
+ | add to list
+ | success message
+ | if "show"
+ \ print out list
+'''
+
+# list of books
+
+
+# welcome message
+print("Welcome to Book Nook!")
+
+print("       ______ ______      ")
+print("     _/      Y      \_    ")
+print("   / / ~Book | ~~ ~  \ \   ")
+print("  / / ~ ~ ~~ |  Nook~ \ \  ")
+print(" / /________.|.________\ \ ")
+print(" \ \--------`-'--------/ / ")
+
+# ask user for input
+'''
+Menu: 
+ Add book (add)
+ Remove book (remove) 
+ Show inventory count (count) 
+ Quit (q)
+ '''
+
+# ask user for book
+
+
+# add to list
+
+
+# add to list
+
+
+# success message
+
+
+# if "show"
+
+
+# print out list
+
